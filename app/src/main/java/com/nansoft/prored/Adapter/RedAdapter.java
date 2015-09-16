64d8db3ce8677c1c33b5f154179f8ada107dac17
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-16 14:49:08 -0600

    Se importa Glide

--- app/src/main/java/com/nansoft/prored/Adapter/RedAdapter.java
+++ app/src/main/java/com/nansoft/prored/Adapter/RedAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.bumptech.glide.Glide;
 import com.nansoft.prored.Model.Red;
 import com.nansoft.prored.R;
