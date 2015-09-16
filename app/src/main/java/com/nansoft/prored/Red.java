cbe07a1e0bfe51d56332d7e7d7a637129a18e946
Susana <su_masis_s@hotmail.com>
2015-09-16 12:53:12 -0600

    Se agrego un comentario

---
+++ app/src/main/java/com/nansoft/prored/Red.java
@@ -0,0 +1,46 @@
+package com.nansoft.prored;
+
+/**
+ * Created by PC on 16/09/2015.
+ */
+public class Red {
+    private String Id;
+    private String Nombre;
+    private String Descripcion;
+    private String UrlImage;
+    private String Id_Encargado;
+
+    public String getId(){
+        return Id;
+    }
+    public void setId(String pId){
+        Id=pId;
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String pNombre){
+        Nombre=pNombre;
+    }
+    public String getDescripcion(){
+        return Descripcion;
+    }
+    public void setDescripcion(String pDescripcion){
+        Descripcion=pDescripcion;
+    }
+    public String getUrlImage(){
+        return UrlImage;
+    }
+    public void setUrlImage(String pUrlImage){
+        UrlImage=pUrlImage;
+    }
+    public String getId_Encargado(){
+        return Id_Encargado;
+    }
+    public void setId_Encargado(String pId_Encargado){
+        Id_Encargado=pId_Encargado;
+    }
+
+
+    ////comentario
+}
