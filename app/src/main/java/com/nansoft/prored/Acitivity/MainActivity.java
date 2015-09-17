2635be5260983089dfdb98c8828e8917e93d8094
Susana <su_masis_s@hotmail.com>
2015-09-17 12:37:30 -0600

    Se creo gridview example un nuevo layoud grid_item y un adapter en java.

--- app/src/main/java/com/nansoft/prored/Acitivity/MainActivity.java
+++ app/src/main/java/com/nansoft/prored/Acitivity/MainActivity.java
@@ -1,43 +1,29 @@
 package com.nansoft.prored.Acitivity;
 
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.nansoft.prored.Adapter.GridAdapter;
 import com.nansoft.prored.R;
 
 public class MainActivity extends Activity {
+    static final String[] numbers = new String[] { "one", "two", "three",
+            "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+            "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen", "eighteen", "nineteen", "twenty", "twenty one",
+            "twenty two" };
     GridView gridView;
+    GridAdapter adapter;
 
-    static final String[] numbers = new String[] {
-            "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine", "ten",
-            "eleven", "twelve", "thirteen", "fourteen",
-            "fifteen","sixteen","seventeen","eighteen",
-            "nineteen","twenty","twenty one","twenty two"
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gridView = (GridView) findViewById(R.id.grid_id);
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, numbers);
-
+        adapter=new  GridAdapter(numbers, this);
         gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 
 }
