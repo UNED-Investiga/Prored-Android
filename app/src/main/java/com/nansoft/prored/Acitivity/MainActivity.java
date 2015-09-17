5e49dbfaedef181da0780919ea4d492850c85877
Susana <su_masis_s@hotmail.com>
2015-09-17 10:22:42 -0600

    Se agrego grid view creamos la clase adapter activity y variables staticas para los numeros con valor string.

--- app/src/main/java/com/nansoft/prored/Acitivity/MainActivity.java
+++ app/src/main/java/com/nansoft/prored/Acitivity/MainActivity.java
@@ -1,41 +1,45 @@
 package com.nansoft.prored.Acitivity;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nansoft.prored.R;
 
+public class MainActivity extends Activity {
+    GridView gridView;
 
-public class MainActivity extends ActionBarActivity {
-
+    static final String[] numbers = new String[] {
+            "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen",
+            "fifteen","sixteen","seventeen","eighteen",
+            "nineteen","twenty","twenty one","twenty two"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.grid_id);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, numbers);
+
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
