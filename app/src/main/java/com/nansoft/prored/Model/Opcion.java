98f14c61bd254ccd15e98411e38e928fd4deec31
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-20 19:27:45 -0600

    Se agrega la clase opción

---
+++ app/src/main/java/com/nansoft/prored/Model/Opcion.java
@@ -0,0 +1,35 @@
+package com.nansoft.prored.Model;
+
+/**
+ * Created by Carlos on 20/09/2015.
+ */
+public class Opcion
+{
+    private String id;
+    private String nombre;
+    private String urlImagen;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+}
