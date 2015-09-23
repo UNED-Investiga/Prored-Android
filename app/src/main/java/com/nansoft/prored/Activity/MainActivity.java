a0e938bb43256bf22a787161bd14626065e20223
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-23 10:18:56 -0600

    Se establece el adapter del grid view

--- app/src/main/java/com/nansoft/prored/Activity/MainActivity.java
+++ app/src/main/java/com/nansoft/prored/Activity/MainActivity.java
@@ -9,24 +9,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nansoft.prored.Adapter.ImageAdapter;
+import com.nansoft.prored.Adapter.OpcionAdapter;
+import com.nansoft.prored.Model.Opcion;
 import com.nansoft.prored.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
     static final String[] numbers = new String[] { "one", "two", "three",
             "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
             "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
             "seventeen", "eighteen", "nineteen", "twenty", "twenty one",
             "twenty two" };
+
     GridView gridView;
     ImageAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gridView = (GridView) findViewById(R.id.grid_id);
-        adapter=new  ImageAdapter(numbers, this);
-        gridView.setAdapter(adapter);
+        //adapter=new  ImageAdapter(numbers, this);
+        OpcionAdapter opcionAdapter = new OpcionAdapter(this,R.layout.item_grid);
+
+        gridView.setAdapter(opcionAdapter);
+
+        for (int i = 0; i <10; i++)
+        {
+            opcionAdapter.add(new Opcion("1","Opcion " + i,"na"));
+        }
+
+
     }
 
 }
