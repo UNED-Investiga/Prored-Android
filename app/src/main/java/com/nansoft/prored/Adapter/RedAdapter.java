f0c743d8d36a4eae2e787f339fb44f35b613d070
Susana <su_masis_s@hotmail.com>
2015-09-29 14:49:46 -0600

    Se corrigio la carpeta de cargar imagen y sin imagen, debido a que aparecian en mipmap.

--- app/src/main/java/com/nansoft/prored/Adapter/RedAdapter.java
+++ app/src/main/java/com/nansoft/prored/Adapter/RedAdapter.java
@@ -73,8 +73,8 @@ public class RedAdapter extends ArrayAdapter<Red>
                 .load(currentItem.getUrlImage().trim())
                 .asBitmap()
                 .fitCenter()
-                .placeholder(R.mipmap.cargar_imagen)
-                .error(R.mipmap.sin_imagen)
+                .placeholder(R.drawable.cargar_imagen)
+                .error(R.drawable.sin_imagen)
                 .into(holder.imgvLogoRed);
 
         return row;
