b806a45da54e28a1c72a356478ee47ae704f69b9
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-10-01 20:27:39 -0600

    Se elimina mensaje de Toast

--- app/src/main/java/com/nansoft/prored/Activity/MainActivity.java
+++ app/src/main/java/com/nansoft/prored/Activity/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity
 
 
 
-                Toast.makeText(getApplicationContext(), objOpcionPresionado.getNombre(), Toast.LENGTH_SHORT).show();
+
 
             }
         });
